c475e45fe205e0601b81c666383b1d92a04f3950
BrigitteAusBurgstaedt <robin260602@gmail.com>
2021-10-14 20:54:00 +0200

    Der Dachs beißt sich in den Schwanz.

--- src/ErsteKlasse.java
+++ src/ErsteKlasse.java
@@ -4,6 +4,7 @@ public class ErsteKlasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Welt");
+		System.out.println("Ich hoffe das klappt jetzt mal!!!");
 	}
 
 }
